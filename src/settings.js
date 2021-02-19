aa90cf458da292afd76fa78cc52b209e23627eb2
ilanKushnir <ilan@zooz.com>
2021-02-19 16:22:10 +0200

    set default settings

--- src/settings.js
+++ src/settings.js
@@ -1,7 +1,7 @@
 const URL = 'https://hermon.pres.global/vouchers';
-const DAYS_TO_CHECK = ['19','20','21','22','23'];
+const DAYS_TO_CHECK = ['20','21','22','23'];
 const ASYNC_CALENDAR_PATH = '//*[@id="app"]/div/main/div/div/div[2]/div[1]/div[2]/div/div/div[2]';
-const ASSERTIONS_INTERVAL = 3000; // milliseconds
+const ASSERTIONS_INTERVAL = 60000; // milliseconds
 
 module.exports = {
     URL,
